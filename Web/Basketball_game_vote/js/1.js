ce8606d1a7f9801d001aac26220446019fb57a35
Urinx <1336006643@qq.com>
2014-10-27 23:24:53 +0800

    update

--- Web/Basketball_game_vote/js/1.js
+++ Web/Basketball_game_vote/js/1.js
@@ -1,26 +1,37 @@
 	function submit(){
 
-		var input=document.querySelectorAll('input');
+		var input=document.querySelectorAll('input[type=checkbox]');
 
 		// Check if is voted
 		if (getCookie('voted')) {
 		//if (0) {
+			setCookie();
 			alert('你已经投过票了,\n一天只能投一次！');
 		}
 		else{
-			var selected=0;
+			var selected=[];
 			for (var i = 0; i < input.length; i++) {
 				if(input[i].checked){
-					selected+=1;
+					//selected+=1;
+					selected.push(input[i].name.toUpperCase());
 				}
 			};
-			if(selected>3){
+			if(selected.length>3){
 					alert('最多只能选择3个！');
 				}
-			else if (selected) {
+			else if (selected.length) {
 				setCookie();
-				// submit
-				document.querySelector('form').submit();
+				
+				// confirm
+				var r=confirm('你选择了：\n'+selected.join(', ')+'\n确定要投吗？');
+				if (r) {
+					// submit
+					setCookie();
+					document.querySelector('form').submit();
+				}
+				else{
+					console.log('Cancel the submit.');
+				};
 			}
 			else{
 				alert('请至少选择一位！');
@@ -37,9 +48,10 @@
 	}
 
 	function setCookie(){
-		// one day expires
+		// daily cookie
 		var d = new Date();
 		d.setTime(d.getTime() + (24*60*60*1000));
+		d.setHours(0,0,0);
 		var expires = "expires="+d.toGMTString();
 		document.cookie='voted=true; path=/; '+expires;
 	}
@@ -47,16 +59,10 @@
 	function li_attach_click(){
 		var li=document.querySelectorAll('li');
 		for (var i = 0; i < li.length; i++) {
-			li[i].flag=0;
 			li[i].querySelector('input[type=checkbox]').checked='';
 			li[i].addEventListener('click',function () {
-				if(this.flag){
-					this.querySelector('input[type=checkbox]').checked='';
-				}
-				else{
-					this.querySelector('input[type=checkbox]').checked='on';
-				}
-				this.flag=1-this.flag;
+				var tmp=this.querySelector('input[type=checkbox]');
+				tmp.checked=tmp.checked?false:true;
 			});
 		};
 	}
@@ -80,7 +86,7 @@
 	window.onload=function(){
 		var b_ul=document.querySelector('#b_ul');
 		var g_ul=document.querySelector('#g_ul');
-		create_li(b_ul,['b0','b1','b5','b6','b7','b10','b11','b12','b13','b17','b99','b18','b30','b45']);
-		create_li(g_ul,['g3','g7','g8','g10','g11','g13','g15','g21','g23','g33','g68','g84']);
+		create_li(b_ul,['b0','b1','b5','b6','b7','b9','b10','b11','b12','b13','b17','b18','b30','b45','b99']);
+		create_li(g_ul,['g3','g7','g8','g10','g11','g13','g15','g21','g23','g33','g68','g84','g98']);
 		li_attach_click();
 	}
