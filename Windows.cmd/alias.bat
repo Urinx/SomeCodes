c0cd8529f8b1ec332895658f51276def6ebab2b1
Urinx <1336006643@qq.com>
2014-11-06 22:48:19 +0800

    windows cmd is too ugly & not handly

---
+++ Windows.cmd/alias.bat
@@ -0,0 +1,5 @@
+@echo off
+doskey ll=ls -al
+doskey rhino=java -jar E:\softs\rhino1_7R4\js.jar
+doskey vi=vim $*
+doskey google="C:\Program Files (x86)\Google\Chrome\Application\chrome" "https://www.google.com.hk/#newwindow=1&safe=strict&q="$*
