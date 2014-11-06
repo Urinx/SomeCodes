fe944b67052882b1dcb73672e48febe5c9903b13
Urinx <1336006643@qq.com>
2014-11-07 07:50:48 +0800

    observe module in python

---
+++ Python/observe.py
@@ -0,0 +1,66 @@
+# /usr/bin/env python
+# coding: utf-8
+
+############
+# 观察者模式
+############
+class AbstractSubject(object):
+    def register(self, listener):
+        raise NotImplementedError("Must subclass me")
+ 
+    def deregister(self, listener):
+        raise NotImplementedError("Must subclass me")
+ 
+    def notify_listeners(self, event):
+        raise NotImplementedError("Must subclass me")
+ 
+class Listener(object):
+    def __init__(self, name, subject):
+        self.name = name
+        subject.register(self)
+ 
+    def notify(self, event):
+        print self.name, "received event", event
+ 
+class Subject(AbstractSubject):
+    def __init__(self):
+        self.listeners = []
+        self.data = None
+ 
+    def getUserAction(self):
+        self.data = raw_input('Enter something to do:')
+        return self.data
+ 
+    # Implement abstract Class AbstractSubject
+ 
+    def register(self, listener):
+        self.listeners.append(listener)
+ 
+    def deregister(self, listener):
+        self.listeners.remove(listener)
+ 
+    def notify_listeners(self, event):
+        for listener in self.listeners:
+            listener.notify(event)
+ 
+ 
+if __name__=="__main__":
+    # make a subject object to spy on
+    subject = Subject()
+ 
+    # register two listeners to monitor it.
+    listenerA = Listener("<listener A>", subject)
+    listenerB = Listener("<listener B>", subject)
+ 
+    # simulated event
+    subject.notify_listeners ("<event 1>")
+    # outputs:
+    #     <listener A> received event <event 1>
+    #     <listener B> received event <event 1>
+ 
+    action = subject.getUserAction()
+    subject.notify_listeners(action)
+    #Enter something to do:hello
+    # outputs:
+    #     <listener A> received event hello
+    #     <listener B> received event hello
