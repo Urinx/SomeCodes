8ab03f7910edf7fe6cb9dc75f1117e7449f9cc34
Urinx <1336006643@qq.com>
2014-12-21 13:48:27 +0800

    o_O

--- Python/mercury.py
+++ Python/mercury.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
-import os,re
+import re,urllib
 
 url='http://admin:27135812@192.168.1.1/userRpm/AssignedIpAddrListRpm.htm'
-r=os.popen('curl '+url)
+r=urllib.urlopen(url)
 content=r.read()
-reg=r'var DHCPDynList = new Array\(([\s\S]*?)\)'
-m=re.findall(reg,content)[0]
-print m
+reg=r'var DHCPDynList = new Array\(\n([\s\S]*?)\n0,0\s\)'
+m=re.findall(reg,content)[0].replace('"','').replace(' ','')
+
+# Outputs
+print '='*80
+print 'Device'+' '*18+'\t   Mac Address   \t     IP     \tTime'
+for line in m.split('\n'):
+    ls=line.split(',')[:-1]
+    le=len(ls[0])
+    if le<24: ls[0]+=' '*(24-le)
+    print '\t'.join(ls)
+print '='*80
