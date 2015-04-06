88f3b100889fb2110f64a625471f58e72ebe1c64
urinx <1336006643@qq.com>
2015-04-06 22:51:49 +0800

    + list of some shell command

---
+++ shell/help.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+
+ls
+pwd
+whoami
+date
+id
+su
+sudo !!
+apt-get install
+whereis python
+cal
+cat help.sh
+cat >>help.sh<<EOF
+shutdown -P 1
+mtr
+nl
+ss
+last
+tree
+pstree
+curl
+links
+kill [PID]
+pgrep -l [keywords]
+ps -eo pid,comm,cmd
+<space>cmd
+stat <filename>
+file <filename>
+echo "this words are auto input to the screen...." | pv -qL 20
+netstat -tl
+netstat -r
+ifconfig
+telnet
+
+sl
+cowsay
+cowsay -f sheep "I too love linux"
+cowsay -l
+rev
+fortune | cowsay
+figlet
+toilet
+toilet -f mono12 -F metal mylinuxbook
+cmatrix
+oneko
+
+less
+tac
+shuf
+top
+look
+aspell -c filename
+arp
+shutdown -P 1
+mtr
+nl
+ss
+last
+tree
+pstree
+curl
+links
+kill [PID]
+pgrep -l [keywords]
+ps -eo pid,comm,cmd
+<space>cmd
+stat <filename>
+file <filename>
+echo "this words are auto input to the screen...." | pv -qL 20
+netstat -tl
+netstat -r
+ifconfig
+telnet
+
+sl
+cowsay
+cowsay -f sheep "I too love linux"
+cowsay -l
+rev
+fortune | cowsay
+figlet
+toilet
+toilet -f mono12 -F metal mylinuxbook
+cmatrix
+oneko
+
+less
+tac
+shuf
+top
+look
+aspell -c filename
+arp
